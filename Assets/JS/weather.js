7a47090821d64f7ca444952881c36d6fb53737e4
DimonStad <57269090+DimonStad@users.noreply.github.com>
2021-11-20 13:19:54 +0300

    React

--- Assets/JS/weather.js
+++ Assets/JS/weather.js
@@ -1,22 +1,29 @@
 
-async function find(){
+async function find(arg){
 	var weather;
-	var city = document.querySelector("#city").value;
-	var link =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d90e9570355188094b55bc4c9b6ea3e9&lang=ru`;
-	await fetch (link)
-	.then((resp)=>{
-		return resp.json();
-		
-	})
-	.then((data)=>{
-		console.log(data);
-		weather = data;
-	})
-	.catch(error=>alert(error.message))
-	var p = document.querySelector("#weather");
-	console.log(weather);
-	p.innerHTML= weather.name + " " + Math.round(weather.main.temp -273.15) ;
-
+	var city = arg;
+	if (!city) {
+		alert("Поле пустое");
+	}
+	else{	
+		var link =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d90e9570355188094b55bc4c9b6ea3e9&lang=ru`;
+		await fetch (link)
+		.then((resp)=>{
+			if (!resp.ok){
+				alert("Город не найден");
+			}
+			else{return resp.json();}			
+		})
+		.then((data)=>{
+			console.log(data);
+			weather = data;
+		})
+		.catch(error=>alert(error.message))
+		var p = document.querySelector("#weather");
+		console.log(weather);
+		p.innerHTML= weather.name + " " + Math.round(weather.main.temp -273.15) + '&deg' + "C";
+		return p.innerHTML;
+	}												
 }
 
 
